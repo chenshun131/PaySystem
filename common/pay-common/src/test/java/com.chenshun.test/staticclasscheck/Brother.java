1db911eeaeb0c53e676017aa52f468b4aa27a3d8
chenshun131 <1539831174@qq.com>
2018-01-30 23:39:30 +0800

    commit static class check

---
+++ common/pay-common/src/test/java/com.chenshun.test/staticclasscheck/Brother.java
@@ -0,0 +1,15 @@
+package com.chenshun.test.staticclasscheck;
+
+/**
+ * User: chenshun131 <p />
+ * Time: 18/1/30 22:13  <p />
+ * Version: V1.0  <p />
+ * Description:  <p />
+ */
+public class Brother extends Super {
+
+    public void doBad() {
+        System.out.println("好好学习");
+    }
+
+}
