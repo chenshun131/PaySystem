2bed58b7d6b43397ad1438c2704ebb06615af0a7
chenshun131 <1539831174@qq.com>
2018-03-21 23:01:34 +0800

    test hash

---
+++ common/pay-common/src/test/java/com.chenshun.test/hash/T1.java
@@ -0,0 +1,17 @@
+package com.chenshun.test.hash;
+
+/**
+ * User: chenshun131 <p />
+ * Time: 18/3/20 23:01  <p />
+ * Version: V1.0  <p />
+ * Description:  <p />
+ */
+public class T1 {
+
+    public static void main(String[] args) {
+        String str = "werewrwer24342";
+        int hash = str.hashCode() & 3;
+        System.out.print(hash);
+    }
+
+}
