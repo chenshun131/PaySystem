0da2c2e0bb05932475c25d091d3a6109910ca6c0
chenshun131 <1539831174@qq.com>
2018-04-15 16:29:38 +0800

    commit canal test class

--- common/pay-common/src/test/java/com.chenshun.test/multipthread/Demo1.java
+++ common/pay-common/src/test/java/com.chenshun.test/multipthread/Demo1.java
@@ -1,16 +1,10 @@
 package com.chenshun.test.multipthread;
 
-<<<<<<< HEAD
 import java.util.concurrent.TimeUnit;
 
 /**
- * User: mew <p />
- * Time: 18/4/10 09:15  <p />
-=======
-/**
  * User: chenshun131 <p />
  * Time: 18/4/8 21:06  <p />
->>>>>>> commit some test class
  * Version: V1.0  <p />
  * Description:  <p />
  */
@@ -22,7 +16,6 @@ public class Demo1 extends Thread {
 
     @Override
     public void run() {
-<<<<<<< HEAD
         while (!interrupted()) {
             System.out.println(getName() + "正在执行线程...");
             try {
@@ -44,17 +37,8 @@ public class Demo1 extends Thread {
 
 //        demo1.stop();
         demo1.interrupt();
-=======
-        System.out.println(getName() + "线程执行了....");
+//        System.out.println(getName() + "线程执行了....");
     }
 
-    public static void main(String[] args) {
-        Demo1 demo1 = new Demo1("demo1-Thread");
-        Demo1 demo2 = new Demo1("demo2-Thread");
-
-        demo1.start();
-        demo2.start();
->>>>>>> commit some test class
-    }
 
 }
--- common/pay-common/src/test/java/com.chenshun.test/multipthread/NewThread.java
+++ common/pay-common/src/test/java/com.chenshun.test/multipthread/NewThread.java
@@ -1,13 +1,10 @@
 package com.chenshun.test.multipthread;
 
 /**
-<<<<<<< HEAD
  * User: mew <p />
  * Time: 18/4/10 09:06  <p />
-=======
  * User: chenshun131 <p />
  * Time: 18/4/8 20:59  <p />
->>>>>>> commit some test class
  * Version: V1.0  <p />
  * Description:  <p />
  */
@@ -21,22 +18,7 @@ public class NewThread implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-<<<<<<< HEAD
             System.out.println("线程执行了......");
-        }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        NewThread newThread = new NewThread();
-        // 初始化线程状态
-        Thread thread = new Thread(newThread); // 创建线程，并指定线程任务
-        thread.start(); // 启动线程
-        while (true) {
-            synchronized (newThread) {
-                Thread.sleep(100);
-                newThread.notifyAll();
-=======
-            System.out.println("自定义的线程执行了....");
         }
     }
 
@@ -55,7 +37,6 @@ public class NewThread implements Runnable {
                     e.printStackTrace();
                 }
                 n.notifyAll();
->>>>>>> commit some test class
             }
         }
     }
