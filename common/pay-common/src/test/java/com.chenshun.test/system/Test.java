962d9e487860af29bec374452a938992bc6a68ba
chenshun131 <1539831174@qq.com>
2018-04-28 09:08:07 +0800

    commit test class

---
+++ common/pay-common/src/test/java/com.chenshun.test/system/Test.java
@@ -0,0 +1,16 @@
+package com.chenshun.test.system;
+
+/**
+ * User: mew <p />
+ * Time: 18/4/27 11:29  <p />
+ * Version: V1.0  <p />
+ * Description:  <p />
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String a = System.getProperty("java.library.path");
+        System.out.println(a);
+    }
+
+}
